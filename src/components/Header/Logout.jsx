4b83b1f6d313ad826c4865b82885bccbed0bafb5
Varun Gurawaliya <varungurawaliya@gmail.com>
2023-12-06 20:02:56 +0530

    first commit

---
+++ src/components/Header/Logout.jsx
@@ -0,0 +1,26 @@
+import React, {useEffect} from 'react'
+import authService from '../../appwrite/auth'
+import { LoadingScreen } from "../";
+import { useDispatch } from 'react-redux'
+import { logout } from '../../store/authSlice'
+
+function Logout() {    
+    
+    const dispatch = useDispatch()
+
+    useEffect(() => {       
+
+    authService.logout().then(()=>{
+      dispatch(logout())
+        
+    })}, [])
+    
+    
+  return (
+    
+      <LoadingScreen/>
+    
+  )
+}
+
+export default Logout
---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './loadingScreen.css'; 
+
+const LoadingScreen = ({display}) => {
+    return (
+      <div className={`${display}`}>
+      <div className={"loading-screen"}>
+        <div className="loading-spinner"></div>
+      </div>
+      </div>
+      
+    );
+};
+
+export default LoadingScreen
