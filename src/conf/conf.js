f60e047e33b15c25073d5ef0eb7c3257e0c5aebc
Varun Gurawaliya <varungurawaliya@gmail.com>
2023-12-06 23:07:11 +0530

    commit

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,9 +1,9 @@
 const conf = {
-    appwriteUrl : String(import.meta.env.VITEAPPWRITEURL),
-    appwriteProjectId : String(import.meta.env.VITEAPPWRITEPROJECTID),
-    appwriteDatabaseId : String(import.meta.env.VITEAPPWRITEDATABASEID),
-    appwriteCollectionId : String(import.meta.env.VITEAPPWRITECOLLECTIONID),
-    appwriteBucketId : String(import.meta.env.VITEAPPWRITEBUCKETID),
+    appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId : String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
     
 }
 
